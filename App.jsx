738236ff8c66dd459dd65acc8ff692b5d46459a4
Swatidash <swatidash2007@gmail.com>
2017-10-25 00:32:33 -0700

    added code

---
+++ App.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+
+class App extends React.Component {
+   render() {
+
+      
+      var myStyle = {
+          fontSize: 60,
+          color: 'white',
+          textAlign: 'center',
+      }
+  var myStyle1 = {
+      fontSize:35,
+      color: 'white',
+      textAlign: 'center',
+      marginTop: -40,
+  }
+  var mystyle2 = {
+      height: 100,
+    width: 400,
+    backgroundColor: 'white',
+    marginLeft: 550,
+    marginTop: 10,
+    textAlign: 'center',
+    color: 'red',
+    display: 'inline-block',
+    
+    
+  }
+
+      return (
+         <div>
+            
+            
+            <h1 style = {myStyle}>Welcome To T-Mobile!</h1>
+            <h3 style = {myStyle1}>We'r excited to meet you</h3>
+           <div style = {mystyle2}>400X200</div>
+           
+         </div>
+         
+      );
+   }
+}
+
+export default App;
