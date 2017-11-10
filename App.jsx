f3ffbdd6ec249ea014d642b72b4b6828a4c91a48
Swatidash <swatidash2007@gmail.com>
2017-11-10 14:02:08 -0800

    added another div

--- App.jsx
+++ App.jsx
@@ -10,37 +10,37 @@ class App extends React.Component {
           color: 'white',
           textAlign: 'center',
       }
-  var myStyle1 = {
-      fontSize:35,
-      color: 'white',
-      textAlign: 'center',
-      marginTop: -40,
+    var myStyle1 = {
+        fontSize:35,
+        color: 'white',
+        textAlign: 'center',
+        marginTop: -40,
   }
-  var mystyle2 = {
-      height: 100,
-    width: 400,
-    backgroundColor: 'white',
-    marginLeft: 550,
-    marginTop: 10,
-    textAlign: 'center',
-    color: 'red',
-    display: 'inline-block',
+    var mystyle2 = {
+        height: 100,
+        width: 400,
+        backgroundColor: 'white',
+        marginLeft: 550,
+        marginTop: 10,
+        textAlign: 'center',
+        color: 'red',
+        display: 'inline-block',
     
     
   }
 
-      return (
-         <div>
+        return (
+            <div>
+                
+                
+                <h1 style = {myStyle}>Welcome To T-Mobile!</h1>
+                <h3 style = {myStyle1}>We'r excited to meet you</h3>
+            <div style = {mystyle2}>400X200</div>
+            <div> This is div 2</div>
+            </div>
             
-            
-            <h1 style = {myStyle}>Welcome To T-Mobile!</h1>
-            <h3 style = {myStyle1}>We'r excited to meet you</h3>
-           <div style = {mystyle2}>400X200</div>
-           
-         </div>
-         
-      );
-   }
-}
+        );
+    }
+    }
 
-export default App;
+    export default App;
